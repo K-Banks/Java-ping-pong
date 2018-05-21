c70a4fb189dd85bc04c8bbafadab59b247228b4c
Kayl Eubanks and Matt Nardoni <kayl@>
2018-05-21 14:58:55 -0700

    Create ping-pong/beep-boop app

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -1,4 +1,23 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PingPong {
+
+    public ArrayList<Object> runPingPong(int countUpTo) {
+        ArrayList<Object> result = new ArrayList<Object>();
+        for (Integer index=1; index<=countUpTo; index++) {
+            if (index % 3 == 0 && index % 5 == 0) {
+                result.add("beepboop");
+            } else if (index % 3 == 0) {
+                result.add("beep");
+            } else if(index % 5 == 0){
+                result.add("boop");
+            } else {
+                result.add(index);
+            }
+        }
+        return result;
+    }
 }
